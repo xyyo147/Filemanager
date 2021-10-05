9b9149b445fe33f41aa061633e24c4191c09321d
xp <1398693166@qq.com>
2021-10-06 00:49:06 +0800

    增加查询功能

--- File manager/src/sample/FAT.java
+++ File manager/src/sample/FAT.java
@@ -5,7 +5,11 @@ import java.util.List;
 
 public class FAT {
     static int[][]  fat = new int[2][64];
-
+    public static int search(int num){
+        int target;
+        target=fat[num/64][num%64];
+        return target;
+    }
 
     public static   int[][] getFAT(){
         fat[0]= (int[]) DiskBlock.getDiskBlock().get(0);
